54aff59b766e330a358783b1b1388e8553671b9f
The-Arkitekt <mr.davidcrumley@gmail.com>
2023-01-05 08:18:03 -0500

    Renamed the sketch and implemented DistributedRobotArchitecture within

--- AutoCartMovementController.ino
+++ AutoCartMovementController.ino
@@ -3,18 +3,24 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include "DistributedRobotArchitecture.h"
 
 //SSID and Password to your ESP Access Point
-const char* ssid = "ESPWebServer";
+const char* ssid = "AutoCartMovementControl";
 //const char* password = "12345678";
 
 ESP8266WebServer server(80); //Server on port 80
+
+//Build robot chassis model
+Chassis robotChassis;
+int driveMotorActuatorId = robotChassis.addActuator(new TestMotor(D2, D4));
+int steeringActuatorId   = robotChassis.addActuator(new TestSteering(D3, D1));
 
 //==============================================================
 //     This rutine is exicuted when you open its IP in browser
 //==============================================================
 void handleRoot() {
-  server.send(200, "text/html", "NodeMCU controller app Root");
+  server.send(200, "text/html", "AutoCart Movement Controller Root");
 }
 
 void handleCommand() { // If a POST request is made to URI /command
@@ -22,7 +28,7 @@ void handleCommand() { // If a POST request is made to URI /command
     server.send(400, "text/plain", "400: Invalid Request");   // The request is invalid, so send HTTP status 400
     return;
   }
-  server.send(200, "text/plain", "got it");
+  server.send(200, "text/plain", "received");
   Serial.println(server.arg("plain"));
 
   String temp = "";
@@ -39,29 +45,34 @@ void handleCommand() { // If a POST request is made to URI /command
   // set outliers to forward for testing
   if(dir > 3 || dir < 0)
     dir = 0;
-  // set outpin based on dir
-  int outpin = 0;
+  
+  Actuator* steering = robotChassis.getActuator(steeringActuatorId);
+  Actuator* driveMotor = robotChassis.getActuator(driveMotorActuatorId);
+
   switch(dir) {
     case(0):
-      outpin = D1;
+      steering->setValue(-1*mag);
+      steering->applyValue();
       break;
     case(1):
-      outpin = D3;
+      driveMotor->setValue(1*mag);
+      driveMotor->applyValue();
       break;
     case(2):
-      outpin = D2;
+      steering->setValue(1*mag);
+      steering->applyValue();
       break;
     case(3):
-      outpin = D4;
+      driveMotor->setValue(-1*mag);
+      driveMotor->applyValue();
       break;
     default:
-      outpin = D3;
+      driveMotor->setValue(0);
+      steering->setValue(0);
+      driveMotor->applyValue();
+      steering->applyValue();
       break;
   }
-
-
-  digitalWrite(outpin, mag);
-
   return;
 }
 
@@ -74,6 +85,7 @@ void handleNotFound(){
 //                  SETUP
 //===============================================================
 void setup(void){
+  // Setup output pins
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
